80236e25bf68d146ed085951308b5da655f8403f
maximeoger <maximeoger93@gmail.com>
2018-06-14 12:08:32 +0200

    Fixes #2

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var ui = {
-    back_to_top : document.getElementById("backtotop")
+    back_to_top : document.getElementById("backtotop"),
+    header : document.getElementById('header-wrapper')
 }
 
 
 document.addEventListener('scroll', function(){
+    console.log(event);
     if( window.pageYOffset > 200 ){
         ui.back_to_top.classList.add('visible');
     }else{
@@ -11,6 +13,16 @@ document.addEventListener('scroll', function(){
     }
 });
 
+document.addEventListener('wheel', function(event){
+    if(event.deltaY >= 0){
+        console.log(event.deltaY);
+        ui.header.classList.add('header-is_hidden');
+    }else{
+        ui.header.classList.remove('header-is_hidden');
+    }
+});
+
 ui.back_to_top.addEventListener('click', function(){
     document.documentElement.scrollTop = document.body.scrollTop = 0;
+    ui.header.classList.remove('header-is_hidden');
 });
